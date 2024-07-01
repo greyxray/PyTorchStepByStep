ea408b04b1ea7287cfa73de12350f56975c750e6
greyxray <greyxray@gmail.com>
2024-07-01 18:44:13 +0300

    ch2: v3, split with datase

--- my_practice/data_preparation.py
+++ my_practice/data_preparation.py
@@ -7,12 +7,21 @@ from generate_data import *
 # define available device
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-# convert x/y train data to pytorch tensors BUT dont send to device
-x_train_tensor = torch.as_tensor(x_train).float()
-y_train_tensor = torch.as_tensor(y_train).float()
+# convert x/y data to pytorch tensors BUT dont send to device
+x_tensor = torch.as_tensor(x).float()
+y_tensor = torch.as_tensor(y).float()
 
 # Define dataset and dataloader for training
-train_dataset = torch.utils.data.TensorDataset(x_train_tensor, y_train_tensor)
+dataset = torch.utils.data.TensorDataset(x_tensor, y_tensor)
+
+# Do the split now on top of the dataset
+train_split_ratio = 0.8
+n_tot = len(dataset)
+n_train = int(n_tot * train_split_ratio)
+n_val = n_tot - n_train
+train_dataset, val_dataset = torch.utils.data.random_split(dataset, [n_train, n_val])
+
+# Init dataloader
 train_loader = torch.utils.data.DataLoader(
     dataset=train_dataset,
     batch_size=16,
--- my_practice/generate_data.py
+++ my_practice/generate_data.py
@@ -11,16 +11,4 @@ epsion = 0.1 * np.random.rand(N,1)  # noise
 x = np.random.rand(N, 1) # matrix [N, 1]
 y = true_w * x + true_b + epsion # y true
 
-# Split
-split_train_ratio = 0.8
-
-indx = np.arange(N)
-np.random.shuffle(indx)
-
-x_train = x[indx[: round(N * split_train_ratio)]]
-y_train = y[indx[: round(N * split_train_ratio)]]
-
-x_val = x[indx[round(N * split_train_ratio) :]]
-y_val = y[indx[round(N * split_train_ratio) :]]
-
 print(f"Done {__file__.__repr__()}")
